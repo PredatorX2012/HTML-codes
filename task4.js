e4f53918cced3dc72cc3354c3f86eaa576a95c44
PredatorX2012 <yusifr626@gmail.com>
2025-05-22 09:23:18 +0400

    Homeworks

---
+++ task4.js
@@ -0,0 +1,14 @@
+let a=15;
+let b=5
+console.log(a*b);
+console.log(a/b);
+console.log(a+b);
+console.log(a-b);
+console.log(a%b);
+console.log(a=b);
+console.log(10<a<20);
+a=a+10
+console.log(a);
+b=b*2
+console.log(b);
+
