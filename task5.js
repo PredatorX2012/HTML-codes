c238a108319a8acb6eebe0aa415a90d51d5bf493
PredatorX2012 <yusifr626@gmail.com>
2025-05-25 13:12:08 +0400

    Homeworks

---
+++ task5.js
@@ -0,0 +1,7 @@
+const fullName='Yusif';
+console.log(fullName.length)
+const firstName='Yusif'
+const lastName='Rza'
+const cumle=`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}`
+console.log(cumle)
+console.log("Sizin adınız "+firstName+" "+lastName+"."+"Tanışlığımıza çox şadam!")
