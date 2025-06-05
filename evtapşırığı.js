3149dbabd7260ae52b9b1ee840dcc32f06c184a1
PredatorX2012 <yusifr626@gmail.com>
2025-06-05 09:03:20 +0400

    homeworks

---
+++ evtapşırığı.js
@@ -0,0 +1,29 @@
+//1-ci tapsiriq
+let a="Salam,Javascript!"
+console.log(a)
+//2-ci tapsiriq
+let b=31;
+let c="salam";
+let d= null;
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log( typeof(g));
+//3-cu tapsiriq
+let x=25;
+let y=5;
+console.log(y+x);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+//4-cu tapsiriq
+let ad="Yusif";
+let yas="13";
+let cumle=`Salam menim adim ${ad}dir.Yasim ${yas}-dur.`;
+console.log(cumle);
+//5-ci tapsiriq
+let name="abuzittin"
+console.log(name.slice(1,5))
+console.log(name.toUpperCase())
+console.log(name.length)
