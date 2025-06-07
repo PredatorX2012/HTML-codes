dff1e0531108cda2c20f0848a524ffcf3bdac1f2
PredatorX2012 <yusifr626@gmail.com>
2025-06-07 20:35:47 +0400

    Homeworks

---
+++ task6.js
@@ -0,0 +1,16 @@
+//1-ci tapsiriq
+let a = "5";
+let b = 5;
+console.log(a == b);
+console.log(a === b);
+// 1-ci de beraberdiler cunku eyni reqem yazilib.
+//2-ci de beraber deyiller cunku biri number,diger string-dir
+//2-ci tapsiriq
+let value = prompt('Reqem daxil edin.')
+console.log("Doğrudur")
+//3-cu tapsiriq
+let username = "admin";
+let password = "1234";
+let user=prompt('Adinizi ve parolunuzu daxil edin:')
+//basa dusmedim bunu😔
+
